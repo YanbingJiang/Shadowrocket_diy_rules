730a1796e6261a22a9e16e034066a499c14047b8
YanbingJiang <YanbingJiang@users.noreply.github.com>
2022-08-13 17:39:04 +0000

    Prettified Code!

--- Archives/Youtube.js
+++ Archives/Youtube.js
@@ -1,8 +1,8 @@
 if ($request.url.indexOf("&oad") != -1) {
-  $done({ response: {body: ""} });
+  $done({ response: { body: "" } });
 } else if ($request.url.indexOf("&ctier") != -1) {
   let url = $request.url.replace(/ctier=[A-Z]/, "ctier=A");
   $done({ response: { status: 302, headers: { Location: url } } });
 } else {
-  $done({})
+  $done({});
 }
